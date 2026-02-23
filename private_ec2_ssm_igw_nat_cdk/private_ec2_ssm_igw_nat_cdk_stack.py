3f2a1d5cbd5fb895d5733dc17b76bc82b0a3fb5e
sanjibborah-pune <sanjibborah26@gmail.com>
2026-02-23 11:56:50 +0530

    added dest ec2

--- private_ec2_ssm_igw_nat_cdk/private_ec2_ssm_igw_nat_cdk_stack.py
+++ private_ec2_ssm_igw_nat_cdk/private_ec2_ssm_igw_nat_cdk_stack.py
@@ -14,7 +14,7 @@ class PrivateEc2SsmIgwNatCdkStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         vpc = ec2.Vpc(self, "MyVpc",
-                max_azs=2,
+                max_azs=1,
                 nat_gateways=1,
                 subnet_configuration=[
                     ec2.SubnetConfiguration(
@@ -38,6 +38,19 @@ class PrivateEc2SsmIgwNatCdkStack(Stack):
             description="Private EC2 accessed only via SSM (no inbound rules)",
             allow_all_outbound=True,
         )
+        sg2 = ec2.SecurityGroup(
+            self,
+            "destinationSg",
+            vpc=vpc,
+            description="destination EC2 security group",
+            allow_all_outbound=True
+        )
+        sg2.add_ingress_rule(
+            peer=sg,
+            connection=ec2.Port.all_icmp(),
+            description="Allow ICMP (ping) from source security group"
+        )
+        
         role = iam.Role(self, "ec2-smm",
                         assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
                         managed_policies=[
@@ -51,10 +64,24 @@ class PrivateEc2SsmIgwNatCdkStack(Stack):
             vpc=vpc,
             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS),
             instance_type=ec2.InstanceType("t3.micro"),
-            machine_image=ec2.MachineImage.latest_amazon_linux(),
+            machine_image=ec2.MachineImage.latest_amazon_linux2(),
             role=role,
             security_group=sg,
             
         )
+        # Create destination EC2 instance in private subnet
+        destinstance = ec2.Instance(
+            self,
+            "DestinationEc2",
+            vpc=vpc,
+            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS),
+            instance_type=ec2.InstanceType("t3.micro"),
+            machine_image=ec2.MachineImage.latest_amazon_linux2(),
+            role=role,
+            security_group=sg2,
+            
+        )
         CfnOutput(self, "InstanceId", value=instance.instance_id)
         CfnOutput(self, "PrivateIp", value=instance.instance_private_ip)
+        CfnOutput(self, "DestinationInstanceId", value=destinstance.instance_id)
+        CfnOutput(self, "DestinationPrivateIp", value=destinstance.instance_private_ip)
